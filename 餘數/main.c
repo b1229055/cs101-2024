e22c11d346fb1f2cb287bad1d52cd0bfb0163658
林敬棠 <89287850+b1229055@users.noreply.github.com>
2024-06-07 10:51:15 +0800

    Update main.c

--- 餘數/main.c
+++ 餘數/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char input[100]; // 用來儲存輸入的字串
+    int a;
+    
+    // 讀取輸入的字串
+    scanf("%s", input);
+    
+    // 解析字串中的整數
+    sscanf(input, "%d", &a);
+    
+    int b = a % 7;
+    printf("%d\n", b);
+    
+    return 0;
+}
 
