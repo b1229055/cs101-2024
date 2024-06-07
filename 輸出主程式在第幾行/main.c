62f07ea9dfb9a28eb3abd48513580bf58070c5ba
林敬棠 <89287850+b1229055@users.noreply.github.com>
2024-06-07 10:52:36 +0800

    Create main.c

---
+++ 輸出主程式在第幾行/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    FILE *fp_in, *fp_out;
+    char line[1000];
+    int l =0;
+    fp_in = fopen("main.c", "r");
+    while (fgets(line, sizeof(line),fp_in)) {
+      l++;
+        if (strstr(line,"int main()")) {
+          break;
+        }
+    }
+    fp_out = fopen("main3.txt", "w");
+    fprintf(fp_out, "%d", l );
+    fclose(fp_in);
+    fclose(fp_out);
+    return 0
